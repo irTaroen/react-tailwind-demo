18b21eaf4a1223c82e523d4078858c1b09aa09ad
Taroen Sitaldin <62523047+irTaroen@users.noreply.github.com>
2023-12-08 13:34:05 +0100

    reinstalled vite and adjusted tranlsate-y

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         className=" flex flex-col-reverse justify-center sm:flex-row p6 items-center gap-8 mb-12 scroll-mt-40"
       >
         <article className=" sm:w-1/2">
-          <h2 className=" max-w-md text-4xl font-bold text-center sm:text-5xl sm:text-left text-slate-900  dark:text-white">
+          <h2 className="max-w-md text-4xl font-bold text-center sm:text-5xl sm:text-left text-slate-900  dark:text-white">
             We Boldy Go{" "}
             <span className=" text-indigo-700">Where No Rocket</span> Has Gone
             Before...
